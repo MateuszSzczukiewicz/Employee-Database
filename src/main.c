709b4593da163c5497d0406556c9ebef66bb6ea9
MateuszSzczukiewicz <szcmat6277@gmail.com>
2025-02-09 19:35:34 +0100

    added handling logic based on flag input

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include "file.h"
 #include "parse.h"
 
-void print_usage(char *argv[0]) {
+void print_usage(char *argv[]) {
   printf("Usage: %s -n -f <database file>\n", argv[0]);
   printf("\t -n  -  create new database file\n");
   printf("\t -f  -  (required) path to database file\n");
@@ -20,7 +20,9 @@ int main(int argc, char *argv[]) {
   bool list = false;
   int c;
 
-  while ((c = getopt(argc, argv, "nf:")) != -1) {
+  int dbfd = -1;
+
+  while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
     switch (c) {
     case 'n':
       newfile = true;
@@ -28,29 +30,43 @@ int main(int argc, char *argv[]) {
     case 'f':
       filepath = optarg;
       break;
-    case '?':
-      printf("Unknown option -%c\n", c);
-      break;
     case 'p':
       portarg = optarg;
       break;
     case 'l':
       list = true;
       break;
+    case '?':
+      printf("Unknown option -%c\n", c);
+      break;
     default:
       return -1;
     }
+  }
 
-    if (filepath == NULL) {
-      printf("Filepath is a required argument\n");
-      print_usage(argv);
-
-      return 0;
-    }
-
-    printf("Newfile: %d\n", newfile);
-    printf("Filepath: %s\n", filepath);
+  if (filepath == NULL) {
+    printf("Filepath is a required argument\n");
+    print_usage(argv);
 
     return 0;
   }
+
+  if (newfile) {
+    dbfd = create_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to create database file\n");
+      return -1;
+    }
+  } else {
+    dbfd = open_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to open database file\n");
+      return -1;
+    }
+  }
+
+  printf("Newfile: %d\n", newfile);
+  printf("Filepath: %s\n", filepath);
+
+  return 0;
 }
